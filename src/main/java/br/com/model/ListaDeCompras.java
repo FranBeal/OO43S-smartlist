5dba194976ef20fc199ad184d92bfc95fcacf537
FranBeal <francielebeal@gmail.com>
2025-04-02 15:05:27 -0300

    aula do dia 26/03/2025, criação do projeto

---
+++ src/main/java/br/com/model/ListaDeCompras.java
@@ -0,0 +1,38 @@
+package br.com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeCompras {
+    private List<Produto> produtos;
+
+    public ListaDeCompras() {
+        produtos = new ArrayList<>();
+    }
+
+    // Adiciona um produto à lista
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    // Remove um produto da lista pelo nome
+    public void removerProduto(String nome) {
+        produtos.removeIf(p -> p.getNome().equalsIgnoreCase(nome));
+    }
+
+    @Override
+    public String toString() {
+        if (produtos.isEmpty()) {
+            return "Lista de compras vazia.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("--- Lista de Compras ---\n");
+
+        // Itera sobre os produtos e adiciona cada um ao StringBuilder
+        for (int i = 0; i < produtos.size(); i++) {
+            sb.append((i + 1)).append(". ").append(produtos.get(i).toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
